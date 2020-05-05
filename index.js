5042db4a2d9eefb9d0cbee59d70e610bbe6b0f6f
imaglazer <avigayilglazer@gmail.com>
2020-05-05 16:04:40 -0400

    switched geolocation to be async, added fetch

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const express = require('express')
 const app = express()
 app.listen(5000, () => console.log('listening at 5000'))
 app.use(express.static('public'))
+app.use(express.json())
+app.post('/api', (request, response) => {
+    console.log(request.body)
+    response.json({
+        status: "success",
+        latitude: request.body.lat,
+        longitude: request.body.lon
+    })
+})
