a94ee0103ef103f4961b78393521186b7fbe16f8
imaglazer <avigayilglazer@gmail.com>
2020-05-07 13:49:49 -0400

    added nedb database

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const Datastore = require('nedb')
+
 const app = express()
 app.listen(5000, () => console.log('listening at 5000'))
 app.use(express.static('public'))
 app.use(express.json())
+
+const database = new Datastore('database.db')
+database.loadDatabase() //this will load the existing database when the file starts 
+database.insert({name: "Blob", status: "slimey"})
+database.insert({name: "Shmear", status: "wet"})
+
 app.post('/api', (request, response) => {
-    console.log(request.body)
+    console.log("request", request.body)
     response.json({
         status: "success",
         latitude: request.body.lat,
